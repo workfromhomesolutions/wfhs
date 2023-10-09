8f135717287f810a7d31daccd8b8f869c6d0ab25
WorkFromHome Solutions <137298140+workfromhomesolutions@users.noreply.github.com>
2023-10-09 18:20:05 -0300

    Update index.js

--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="name='p:domain_verify' content='47bff4edec16c6dfc4c828b9dc1605d4'">
+      p:domain_verify="47bff4edec16c6dfc4c828b9dc1605d4">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
